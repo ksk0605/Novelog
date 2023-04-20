863f05c8c9c749d688b01ce1c5b97f44bd28613f
kimsungkyum <sungkyum1@naver.com>
2023-04-20 18:33:46 +0900

    feat: fist upload

---
+++ src/main/java/com/novelog/NovelogApplication.java
@@ -0,0 +1,13 @@
+package com.novelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovelogApplication.class, args);
+    }
+
+}
